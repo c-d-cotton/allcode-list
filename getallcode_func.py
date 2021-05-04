1f8cc86d8c75ff47cfac2f04d848d6b46eeed432
Christopher Cotton <cdc2175@columbia.edu>
2021-05-04 18:09:18 -0400

    Update how process argparse

--- getallcode_func.py
+++ getallcode_func.py
@@ -120,7 +120,7 @@ def getallcode_ap():
     args=parser.parse_args()
     #End argparse:}}}
 
-    filelist = process_fileinputs(args.filename, args.files_asstring, args.files_aslines, args.files_infile, args.files_indir, args.files_inpwd)
+    filelist = process_fileinputs(args)
 
     # remove comments and blank lines in parselist
     filelist = [filename for filename in filelist if not filename.startswith('#') and not filename == '']
